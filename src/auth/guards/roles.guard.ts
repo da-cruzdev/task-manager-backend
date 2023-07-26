246a4a722cd23387bc7b389d5fba8b4c6eebbaf2
Da-cruz <wilfrieddacruz@arolitec.com>
2023-07-26 16:52:56 +0000

    create a role guard

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from 'src/common/enums/user-role.enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const cxt = GqlExecutionContext.create(context);
+    const { user } = cxt.getContext().req;
+
+    if (!user || !user.role) {
+      return false;
+    }
+    return requiredRoles.some((role) => user.role === role);
+  }
+}
